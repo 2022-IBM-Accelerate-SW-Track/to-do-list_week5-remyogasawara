e0c7125083a5fe99b71093788b2c1e0aeb0b7b76
remyogasawara <remyogasawara@gmail.com>
2022-07-13 20:26:10 -0700

    update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import profile_pic_remy from "../assets/profile_pic_remy.jpg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            // Image goes here            
+            alt="Profile Pic"
+            src={profile_pic_remy}
             ></img>
-          </div>
         </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
+      </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title"><p>Remy Ogasawara</p></div>
+          <div className="brief_description">
+            <p>
+            I am a second year Computer Science major at the University of California, Santa Cruz.
+            I also love to crochet, cook and garden. 
+            A fun fact about me is that my family owns bees in our backyard.
+            </p>
           </div>
         </div>
       </div>
-    );
+    </div>
+  
+    )
   }
 }
